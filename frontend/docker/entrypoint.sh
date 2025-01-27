2f9e098c71ca7aa1517a881d4cd88167be8e5cda
delciocapolo <delciocapolo096@gmail.com>
2025-01-27 08:23:32 +0100

    feat: added poll to force angular to check if have any change in files

--- frontend/docker/entrypoint.sh
+++ frontend/docker/entrypoint.sh
@@ -10,7 +10,7 @@ if [ "$NODE_ENV" = "production" ]; then
     ng build && npm run serve:ssr:frontend
 elif [ "$NODE_ENV" = "development" ]; then
     echo "Running development Angular Application"
-    ng serve --host=0.0.0.0
+    ng serve --host=0.0.0.0 --poll 2000
 else
     echo "NODE_ENV not set or unknown. Falling back to default."
     ng build && npm run serve:ssr:frontend
