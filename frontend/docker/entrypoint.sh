e5617130f75ee28aa755e460d36f08551d37283d
delciocapolo <delciocapolo096@gmail.com>
2025-01-28 08:13:16 +0100

    feat: tryin 9

--- frontend/docker/entrypoint.sh
+++ frontend/docker/entrypoint.sh
@@ -7,11 +7,11 @@ echo "Angular analytics is off"
 
 if [ "$NODE_ENV" = "production" ]; then
     echo "Running production Angular Application"
-    ng build && npm run serve:ssr:frontend
+    npm run build:prod
 elif [ "$NODE_ENV" = "development" ]; then
     echo "Running development Angular Application"
     ng serve --host=0.0.0.0 --poll 2000
 else
     echo "NODE_ENV not set or unknown. Falling back to default."
-    ng build && npm run serve:ssr:frontend
+    npm run build:prod
 fi
