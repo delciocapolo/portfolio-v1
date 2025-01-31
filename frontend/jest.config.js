ff0fa5615ef9bdfcf18b7982f992f4f6876441f3
delciocapolo <delciocapolo096@gmail.com>
2025-01-31 12:33:19 +0100

    test: ouput path

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,7 +2,10 @@ export default {
   preset: "jest-preset-angular",
   setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
   testMatch: ["**/__tests__/**/*.spec.ts", "**/?(*.)+(spec|test).ts"],
-  reporters: ["default", "jest-junit"],
+  reporters: [
+    "default",
+    ["jest-junit", { outputDirectory: "coverage", outputName: "junit.xml" }],
+  ],
   coverageDirectory: "coverage",
   moduleNameMapper: {
     "^@app/(.*)$": "<rootDir>/src/app/$1",
