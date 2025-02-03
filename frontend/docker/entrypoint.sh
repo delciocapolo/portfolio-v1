0cc1f6ca376573c63383f29af8ed80918a75a4b2
delciocapolo <delciocapolo096@gmail.com>
2025-02-03 16:27:10 +0100

    feat: undo command to build and serve builded site

--- frontend/docker/entrypoint.sh
+++ frontend/docker/entrypoint.sh
@@ -7,11 +7,11 @@ echo "Angular analytics is off"
 
 if [ "$NODE_ENV" = "production" ]; then
     echo "Running production Angular Application"
-    npm run build
+    npm run build:prod
 elif [ "$NODE_ENV" = "development" ]; then
     echo "Running development Angular Application"
     ng serve --host=0.0.0.0 --poll 2000
 else
     echo "NODE_ENV not set or unknown. Falling back to default."
-    npm run build
+    npm run build:prod
 fi
