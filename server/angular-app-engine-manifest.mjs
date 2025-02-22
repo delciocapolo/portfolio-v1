3153e31beca3a23174ded222757e5ea37988d5f5
delciocapolo <delciocapolo096@gmail.com>
2025-02-22 20:35:50 +0100

    Auto-generated commit

--- server/angular-app-engine-manifest.mjs
+++ server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/portfolio-v1',
+  basePath: '/',
   supportedLocales: {
   "en-US": ""
 },
--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -2,13 +2,13 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/portfolio-v1/',
+  baseHref: '/',
   locale: undefined,
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 2999, hash: '78d0c95292b7190ad6c8ccf7dc04a7e5a36e4cbcb28e2ad54be51197f32b16c1', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1050, hash: 'b0ba3394912361c0236ee32147d05e50887ceb055192c17b97d0cd05d8f3dff6', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 2986, hash: '807fa65bfdb63698a6eeddc8a69c64f33cfcf76109eed6805793b533b94b87cd', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1037, hash: '535b3cf85d2d541b489e9da53020c57c33a52e93a339d592fabc422a30693162', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-OQDFXMHY.css': {size: 3774, hash: 'ujbVCxuxkOo', text: () => import('./assets-chunks/styles-OQDFXMHY_css.mjs').then(m => m.default)}
   },
 };
