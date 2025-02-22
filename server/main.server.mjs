e57841596291481d01367b86fb1eb1ee5cadb41a
delciocapolo <delciocapolo096@gmail.com>
2025-02-22 23:57:42 +0100

    Auto-generated commit

--- server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a as r,f as e,g as t,i as o,j as p,k as m}from"./chunk-C6TOJ7BI.mjs";import"./chunk-AFOT676I.mjs";import a from"./angular-app-manifest.mjs";e(a);export{m as default,p as \u0275destroyAngularServerApp,t as \u0275extractRoutesAndCreateRouteTree,o as \u0275getOrCreateAngularServerApp,r as \u0275resetCompiledComponents};
